e25563a46ab15c7c38b77c35247e4a8ff938e30d
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-05-24 09:58:11 +0200

    Rewrite it using pointers

--- Lab 9/Polygon/Main.cpp
+++ Lab 9/Polygon/Main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include "Rectangle.h"
 #include "Triangle.h"
-
 using namespace std;
-int main() {
-Rectangle rect;
-Triangle trgl;
-rect.SetValues(4,5);
-trgl.SetValues (4,5);
-cout << rect.Area() << '\n';
-cout << trgl.Area() << '\n';
-return 0;
+int main()
+{
+    Rectangle rect;
+    Triangle trgl;
+    Polygon *pPoly1 = &rect;
+    Polygon *pPoly2 = &trgl;
+    pPoly1->SetValues(4, 5);
+    pPoly2->SetValues(4, 5);
+    cout << rect.Area() << '\n';
+    cout << trgl.Area() << '\n';
+    return 0;
 }
