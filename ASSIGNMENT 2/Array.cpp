fa16220ea7136af47253485328dd987900d4f6b1
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-06-05 22:38:41 +0200

    Changed the logic to use square bracket notation for arrays instead of indirection operator -Added comments for readability

--- ASSIGNMENT 2/Array.cpp
+++ ASSIGNMENT 2/Array.cpp
@@ -7,23 +7,31 @@ int main()
     cout << "Enter number of rows (between 1 and 4): ";
     cin >> row;
 
+    // Check if input is btwn 1 and 3
     while (row < 1 || row > 3)
     {
-        cout << "Enter number of rows (between 1 and 4): ";
+        cout << "Enter number of rows (between 1 and 3): ";
         cin >> row;
     }
 
-    cout << "Enter number of columns (between 1 and 4): ";
+    cout << "Enter number of columns (between 1 and 3): ";
     cin >> col;
 
+    // Check if input is btwn 1 and 3
     while (col < 1 || col > 3)
     {
-        cout << "Enter number of columns (between 1 and 4): ";
+        cout << "Enter number of columns (between 1 and 3): ";
         cin >> col;
     }
 
-    // Dynamic allocate memory of  a 2D array
-    int *array = new int(row * col * sizeof(int));
+    // Dynamic allocate memory of a row of an array
+    int** array = new int*[row];
+
+    // Dynamic allocate memory of col for each row;
+    for (int i = 0; i < row; i++){
+        array[i] = new int[col];
+    }
+
 
     // Initialize elements of array 
     int count = 0;
@@ -32,8 +40,7 @@ int main()
         for (int j = 0; j < col; j++)
         {
             cout << "Enter number: ";
-            cin >> *(array + count);
-            count++;
+            cin >> array[i][j];
         }
         cout << endl;
     }
@@ -44,8 +51,7 @@ int main()
     {
         for (int j = 0; j < col; j++)
         {
-            cout << *(array + count) << " ";
-            count++;
+            cout << array[i][j] << " ";
         }
         cout << endl;
     }
