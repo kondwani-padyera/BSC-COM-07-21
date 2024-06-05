3979a1bf7c6fd0fcd00884e38c6ea9f5ac6fc4e4
kondwani-padyera <bsc-com-07-21@unima.ac.mw>
2024-06-05 22:48:49 +0200

    Get string input using getline not cin -Added comments for readability -Dynamically allocate mmemory for string using string object instead of as array of character pointers

--- ASSIGNMENT 2/MemoryAllocation.cpp
+++ ASSIGNMENT 2/MemoryAllocation.cpp
@@ -3,24 +3,28 @@ using namespace std;
 
 int main()
 {
-    // Dinamic allocation of integer
-    int *num = new int(sizeof(int));
-    char *text = new char[100];
+    // Dynamic allocation of an integer
+    int *num = new int;
 
-    // Dinamic allocation of a string
-    
+    // Dynamic allocation of a string
+    string *text = new string;
+
     // Get user input
     cout << "Enter a number: ";
     cin >> *num;
-    cout << "Enter a word: ";
-    cin >> text;
 
-    // Value of integer
+    // Close the cin object
+    cin.ignore();
+    cout << "Enter a text: ";
+
+    // Get a whole line from the user instead of the first word as in the case of cin
+    getline(cin, *text);
+
+    // Printing the values of the variables
     cout << "The value of num is: " << *num << endl;
-    cout << "The value of word is: " << text;
-
+    cout << "The value of text is: " << *text;
 
     // Deallocate memory
     delete num;
-    delete[] text;
+    delete text;
 }
